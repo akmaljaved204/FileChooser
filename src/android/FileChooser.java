183367619d024ba5fd28920ba328315b4ad400dd
akmaljaved204 <fortsolution@.(none)>
2015-02-14 18:13:18 +0500

    02/14/2015 orignal with change

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -11,6 +11,16 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import android.content.Context;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.database.Cursor;
+
+
 public class FileChooser extends CordovaPlugin {
 
     private static final String TAG = "FileChooser";
@@ -79,4 +89,53 @@ public class FileChooser extends CordovaPlugin {
             }
         }
     }
+	public String getRealPathFromURI (Context context ,Uri contentUri)throws Exception
+	{
+	     String path = null;
+	     String[] proj = { MediaStore.MediaColumns.DATA };
+
+	        if("content".equalsIgnoreCase(contentUri.getScheme ()))
+	            {
+	                Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+	                if (cursor.moveToFirst()) {
+	                    int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+	                    path = cursor.getString(column_index);
+	                }
+	                cursor.close();
+	                return path;
+	            }
+	            else if("file".equalsIgnoreCase(contentUri.getScheme()))
+	            {
+	                return contentUri.getPath();
+	            }
+	            return null;
+	}
+		
+	private String encodeFileToBase64Binary(String fileName) throws IOException {
+		File file = new File(fileName);
+		byte[] bytes = loadFile(file);
+		byte[] encoded = Base64.encode(bytes, Base64.DEFAULT);
+		String encodedString = new String(encoded);
+		return encodedString;
+	}
+	private byte[] loadFile(File file) throws IOException {
+	    InputStream is = new FileInputStream(file);
+	    long length = file.length();
+	    if (length > Integer.MAX_VALUE) {
+	        // File is too large
+	    }
+	    byte[] bytes = new byte[(int)length];
+	    int offset = 0;
+	    int numRead = 0;
+	    while (offset < bytes.length
+	           && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+	        offset += numRead;
+	    }
+	    if (offset < bytes.length) {
+	        throw new IOException("Could not completely read file "+file.getName());
+	    }
+ 
+	    is.close();
+	    return bytes;
+	}
 }
